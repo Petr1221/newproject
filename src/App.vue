f271faff8926c60daf87dd76e5a02a7be2167863
Petr Smirnov <120395181+Petr1221@users.noreply.github.com>
2022-12-21 09:25:01 +0000

    vue1

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@ export default {
   name: 'App',
   components: {
     BarChart
+  },
+created() {
+    this.axios
+      .get('data.json')
+      .then(response => {
+      console.log (response.data)
+      })
   }
 }
 </script>
